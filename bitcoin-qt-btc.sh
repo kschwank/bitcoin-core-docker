a3f824d3bbce624833ef4d2fa622cfdaf416a88e
Karsten Schwank <karsten.schwank@gmail.com>
2018-07-13 19:17:16 +0200

    use uid/gid

--- bitcoin-qt-btc.sh
+++ bitcoin-qt-btc.sh
@@ -3,5 +3,5 @@ XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker-xauth
 mkdir -p ${HOME}/.bitcoin-btc
 xauth nlist ${DISPLAY} | sed -e 's/^..../ffff/' | xauth -f ${XAUTH} nmerge -
-docker run -ti --rm -e "XAUTHORITY=${XAUTH}" -e "DISPLAY=${DISPLAY}" -v ${HOME}/.bitcoin-btc:/bitcoin -v ${XAUTH}:${XAUTH} -v ${XSOCK}:${XSOCK} --name="bitcoin-qt-btc" --user="1000:1000" kschwank/bitcoin-core-qt
+docker run -ti --rm -e "XAUTHORITY=${XAUTH}" -e "DISPLAY=${DISPLAY}" -v ${HOME}/.bitcoin-btc:/bitcoin -v ${XAUTH}:${XAUTH} -v ${XSOCK}:${XSOCK} --name="bitcoin-qt-btc" --user="${UID}:${GID}" kschwank/bitcoin-core-qt
 
